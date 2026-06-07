35467f09dba25298ca494b5fcc5e02be52db6287
Luca Jannsen <69324615+Louuke@users.noreply.github.com>
2026-06-07 14:42:02 +0200

    fix: reverse proxy for matching service

--- web-ui/vite.config.ts
+++ web-ui/vite.config.ts
@@ -7,7 +7,8 @@ export default defineConfig({
   plugins: [react()],
   server: {
     proxy: {
-      '/api/workflow': 'https://msa26-peer-review.fh-wedel.dev'
+      '/api/workflow': 'https://msa26-peer-review.fh-wedel.dev',
+      '/api/matching': 'https://msa26-peer-review.fh-wedel.dev'
     }
   }
 })
