fcfc19eb7ca57e2ac813f328e9d71440b3d5838a
MarcelOs29 <marcel.os.ham@gmail.com>
2026-06-12 15:25:15 +0200

    fix package

--- communicationService/src/main/java/com/fh_wedel/communication/config/SecurityConfig.java
+++ communicationService/src/main/java/com/fh_wedel/communication/config/SecurityConfig.java
@@ -1,3 +1,5 @@
+package com.fh_wedel.communication.config;
+
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
