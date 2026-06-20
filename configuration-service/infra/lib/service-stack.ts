c0d63fa85d0661fbb7472d76132c6246a3a6cd2a
Luca Jannsen <69324615+Louuke@users.noreply.github.com>
2026-06-20 18:14:21 +0200

    feat: allow ingress on port 8081 for configuration-service

--- configuration-service/infra/lib/service-stack.ts
+++ configuration-service/infra/lib/service-stack.ts
@@ -109,6 +109,7 @@ export class ServiceStack extends cdk.Stack {
 
     if (props.enablePublicIpV4) {
       ecsSecurityGroup.addIngressRule(ec2.Peer.anyIpv4(), ec2.Port.tcp(containerPort), 'Inbound HTTP IPv4');
+      ecsSecurityGroup.addIngressRule(ec2.Peer.anyIpv4(), ec2.Port.tcp(8081), 'Inbound HTTP IPv4 on port 8081');
       ecsSecurityGroup.addIngressRule(ec2.Peer.anyIpv4(), ec2.Port.tcp(443), 'Inbound HTTPS IPv4');
       ecsSecurityGroup.addEgressRule(ec2.Peer.anyIpv4(), ec2.Port.tcp(80), 'Outbound HTTP IPv4');
       ecsSecurityGroup.addEgressRule(ec2.Peer.anyIpv4(), ec2.Port.tcp(443), 'Outbound HTTPS IPv4');
@@ -121,6 +122,10 @@ export class ServiceStack extends cdk.Stack {
     const lambdaSg = ec2.SecurityGroup.fromSecurityGroupId(this, 'LambdaSg', lambdaSgId);
     ecsSecurityGroup.addIngressRule(lambdaSg, ec2.Port.tcp(containerPort), 'Allow incoming traffic from API Gateway proxy Lambda');
     ecsSecurityGroup.addIngressRule(ec2.Peer.anyIpv6(), ec2.Port.tcp(containerPort), 'Inbound HTTP IPv6 from VPC services (ECS-to-ECS via AAAA record)');
+    
+    // Also allow port 8081
+    ecsSecurityGroup.addIngressRule(lambdaSg, ec2.Port.tcp(8081), 'Allow incoming traffic from API Gateway proxy Lambda on port 8081');
+    ecsSecurityGroup.addIngressRule(ec2.Peer.anyIpv6(), ec2.Port.tcp(8081), 'Inbound HTTP IPv6 from VPC services on port 8081');
 
     const cloudMapNamespace = ImportedRessources.getCloudMapNamespace(this);
     const sdService = EcsInfra.createServiceDiscoveryAAAARecord(this, props.serviceName, cloudMapNamespace);
